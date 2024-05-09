e2ef5777dbe289279065a98352e43203c603db5f
MMETehrani <mmtehrani36@gmail.com>
2024-05-09 16:47:53 +0330

    add color

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,6 @@ import 'package:hexcolor/hexcolor.dart';
 
 final Color textGreen = HexColor('#ffffff');
 final Color textGrey = Color(0xFF464646);
+final Color textReuslt = Color(0xFFADA09B);
 final Color backgroundGrey = Colors.white;
 final Color backgroundGreyDark = Color(0xFFFF875D);
