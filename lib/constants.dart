0c6eae508f0eda72bca1cf9fae27a985b2772ea6
MMETehrani <mmtehrani36@gmail.com>
2024-05-12 21:16:48 +0330

    add ColorScheme

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-final Color textGreen = HexColor('#ffffff');
-final Color textGrey = Color(0xFF464646);
-final Color textReuslt = Color(0xFFADA09B);
-final Color backgroundGrey = Colors.white;
-final Color backgroundGreyDark = Color(0xFFFF875D);
+// final Color textGreen = HexColor('#ffffff');
+final Color primary = HexColor('#fe875d');
+final Color secondary = HexColor('#ffffff');
+final Color onPrimary = HexColor('#464646');
+final Color onSecondary = HexColor('#ffffff');
+final Color primaryContainer = HexColor('#fdf3f1');
+final Color onBackground = HexColor('#464646');
+final Color background = HexColor('#fce5df');
+// final Color textGrey = Color(0xFF464646);
+// final Color textReuslt = Color(0xFFADA09B);
+// final Color backgroundGrey = Colors.white;
+// final Color backgroundGreyDark = Color(0xFFFF875D);
+
+
